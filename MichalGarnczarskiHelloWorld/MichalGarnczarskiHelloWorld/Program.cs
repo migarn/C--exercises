0a73ecb8ab89ec4266b68be57a56ac490d2d43cd
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-25 13:35:17 +0200

    Catching exception added

--- MichalGarnczarskiHelloWorld/MichalGarnczarskiHelloWorld/Program.cs
+++ MichalGarnczarskiHelloWorld/MichalGarnczarskiHelloWorld/Program.cs
@@ -6,9 +6,16 @@ namespace MichalGarnczarskiHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wpisz coś...");
-            int userValue = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Wartość int wpisana przez użytkownika to: \"{userValue}\".");
+            Console.WriteLine("Podaj liczbę:");
+            try
+            {
+                int userValue = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Wartość int wpisana przez użytkownika to: \"{userValue}\".");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Podana wartość jest nieprawidłowa!");
+            }
             Console.ReadKey();
         }
     }
