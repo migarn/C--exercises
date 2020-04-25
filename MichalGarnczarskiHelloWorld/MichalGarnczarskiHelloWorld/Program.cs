e0770ad7b95cd25dba89509e1363280de0f9490c
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-25 14:40:27 +0200

    Factorial implementation added

--- MichalGarnczarskiHelloWorld/MichalGarnczarskiHelloWorld/Program.cs
+++ MichalGarnczarskiHelloWorld/MichalGarnczarskiHelloWorld/Program.cs
@@ -26,33 +26,52 @@ namespace MichalGarnczarskiHelloWorld
             {
                 Console.WriteLine("Wpisz pierwszą liczbę:");
                 double firstNumber = double.Parse(Console.ReadLine());
-                Console.WriteLine("Wpisz drugą liczbę:");
-                double secondNumber = double.Parse(Console.ReadLine());
-                Console.WriteLine("Wpisz znak działania (\"+\" albo \"-\" albo \"*\" albo \":\"):");
-                string operation = Console.ReadLine();
-
-                switch (operation)
+                Console.WriteLine("Wpisz drugą liczbę lub wpisz \"!\", żeby policzyć silnię:");
+                String input = Console.ReadLine();
+                if (!input.Equals("!"))
                 {
-                    case "+":
-                        Console.WriteLine("Suma podanych liczb jest równa: " + (firstNumber + secondNumber));
-                        break;
-                    case "-":
-                        Console.WriteLine("Różnica podanych liczb jest równa: " + (firstNumber - secondNumber));
-                        break;
-                    case "*":
-                        Console.WriteLine("Iloczyn podanych liczb jest równy: " + (firstNumber * secondNumber));
-                        break;
-                    case ":":
-                        Console.WriteLine("Iloraz podanych liczb jest równa: " + (firstNumber / secondNumber));
-                        break;
-                    default:
-                        throw new Exception();
+                    double secondNumber = double.Parse(input);
+                    Console.WriteLine("Wpisz znak działania (\"+\" albo \"-\" albo \"*\" albo \":\"):");
+                    string operation = Console.ReadLine();
+
+                    switch (operation)
+                    {
+                        case "+":
+                            Console.WriteLine("Suma podanych liczb jest równa: " + (firstNumber + secondNumber));
+                            break;
+                        case "-":
+                            Console.WriteLine("Różnica podanych liczb jest równa: " + (firstNumber - secondNumber));
+                            break;
+                        case "*":
+                            Console.WriteLine("Iloczyn podanych liczb jest równy: " + (firstNumber * secondNumber));
+                            break;
+                        case ":":
+                            Console.WriteLine("Iloraz podanych liczb jest równa: " + (firstNumber / secondNumber));
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Silnia podanej liczby jest równa: " + factorial(firstNumber));
                 }
             }
             catch (Exception e)
             {
                 Console.WriteLine("Coś poszło nie tak...");
             }
+        }
+
+        private static double factorial(double number) {
+            if (number == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * factorial(number - 1);
+            }      
         }
     }
 }
