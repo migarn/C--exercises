44b9549354c8fa3a8a7984f212027ca49eff69bf
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-04-25 13:46:21 +0200

    Handling excpetion added to double summing

--- MichalGarnczarskiHelloWorld/MichalGarnczarskiHelloWorld/Program.cs
+++ MichalGarnczarskiHelloWorld/MichalGarnczarskiHelloWorld/Program.cs
@@ -6,11 +6,18 @@ namespace MichalGarnczarskiHelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Podaj pierwszą liczbę:");
-            double firstNumber = double.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj drugą liczbę:");
-            double secondNumber = double.Parse(Console.ReadLine());
-            Console.WriteLine("Suma podanych liczb jest równa: " + (firstNumber + secondNumber));
+            try
+            {
+                Console.WriteLine("Podaj pierwszą liczbę:");
+                double firstNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Podaj drugą liczbę:");
+                double secondNumber = double.Parse(Console.ReadLine());
+                Console.WriteLine("Suma podanych liczb jest równa: " + (firstNumber + secondNumber));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Coś poszło nie tak...");
+            }
             Console.ReadKey();            
         }
     }
