0a889b86214bdef8e9beea7bfa55270c33760b58
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-16 11:46:11 +0200

    Method hello and its view added

--- MichalGarnczarskiMVCHelloWorld/Controllers/HomeController.cs
+++ MichalGarnczarskiMVCHelloWorld/Controllers/HomeController.cs
@@ -33,5 +33,10 @@ namespace MichalGarnczarskiMVCHelloWorld.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public string Hello()
+        {
+            return "Cała naprzód";
+        }
     }
 }
