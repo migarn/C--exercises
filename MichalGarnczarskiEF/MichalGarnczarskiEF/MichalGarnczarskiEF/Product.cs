16826c7569157b036b45fd05d14fe3cb10aaf7f8
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-16 09:23:51 +0200

    EF project initialized

---
+++ MichalGarnczarskiEF/MichalGarnczarskiEF/MichalGarnczarskiEF/Product.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MichalGarnczarskiEF
+{
+    class Product
+    {
+        public int ProductID { get; set; }
+    }
+}
