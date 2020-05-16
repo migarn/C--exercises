891e52233278e935f50073f285f5bcf429b4fed4
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-16 10:42:16 +0200

    Migrations added

---
+++ MichalGarnczarskiEF/MichalGarnczarskiEF/MichalGarnczarskiEF/Migrations/20200516084059_InitialProductCreation.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MichalGarnczarskiEF.Migrations
+{
+    public partial class InitialProductCreation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    UnitsInStock = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.ProductID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
