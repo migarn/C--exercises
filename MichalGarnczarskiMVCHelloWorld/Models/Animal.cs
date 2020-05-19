336bc6630e6ea48824b823b24016825221cb284e
Michal Garnczarski <michalgarnczarski@gmail.com>
2020-05-19 18:41:14 +0200

    Annotations added to Animal's BirthDate

--- MichalGarnczarskiMVCHelloWorld/Models/Animal.cs
+++ MichalGarnczarskiMVCHelloWorld/Models/Animal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,9 @@ namespace MichalGarnczarskiMVCHelloWorld.Models
     {
         public int ID { get; set; }
         public string Name { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
     }
 }
